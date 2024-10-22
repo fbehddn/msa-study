3a2ebd2b6a24c6999d37419c47614d0b7a45c316
fbehddn <tom0611@naver.com>
2024-10-22 23:38:27 +0900

    Section3 - API Gateway Service - Spring Cloud Gateway Load Balancer

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,6 +1,9 @@
 package com.example.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to First Service!";
@@ -24,7 +34,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service in + PORT %s.", env.getProperty("local.server.port"));
     }
 }
