b162780dd8d6960b895a53a5b7656d4214ebf99a
fbehddn <tom0611@naver.com>
2024-10-28 16:56:54 +0900

    Section6 - Catalogs and Order Microservice - (Users Microservice) Spring cloud Gateway 연동 및 사용자 조회 기능 추가

--- user-service/src/main/java/com/example/userservice/dto/UserDto.java
+++ user-service/src/main/java/com/example/userservice/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.example.userservice.dto;
 
+import com.example.userservice.vo.ResponseOrder;
+import java.util.List;
 import lombok.Data;
 
 import java.util.Date;
@@ -12,7 +14,7 @@ public class UserDto {
     private String userId;
     private Date createdAt;
 
-    private String decryptedPwd;
-
     private String encryptedPwd;
+
+    private List<ResponseOrder> orders;
 }
--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,11 +15,15 @@ public class WebSecurity {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                                .requestMatchers("/users/**").permitAll()
-//                        .anyRequest().authenticated()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/**").permitAll()
                 )
+                .headers(headers -> headers
+                        .defaultsDisabled()
+                        .frameOptions(FrameOptionsConfig::sameOrigin))
                 .build();
     }
 }
