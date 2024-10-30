17825f4382e5ccf3b54a1452272387ef99e9f047
fbehddn <tom0611@naver.com>
2024-10-30 15:02:34 +0900

    Section 07 - Users Microservice (2) - JWT 발급 및 처리

--- user-service/src/main/java/com/example/userservice/security/CustomAuthenticationFilter.java
+++ user-service/src/main/java/com/example/userservice/security/CustomAuthenticationFilter.java
@@ -4,12 +4,18 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.service.UserService;
 import com.example.userservice.vo.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Date;
+import javax.crypto.SecretKey;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -49,6 +55,19 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 //        super.successfulAuthentication(request, response, chain, authResult);
         String userName = ((User) authResult.getPrincipal()).getUsername();
         UserDto userDetails = userService.getUserDetailsByEmail(userName);
+
+        String secretKey = env.getProperty("token.secret");
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        String token = Jwts.builder()
+                .setSubject(userDetails.getUserId())
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(
+                        env.getProperty("token.expiration_time"))))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+
+        response.addHeader("token", token);
+        response.addHeader("userId", userDetails.getUserId());
     }
 
 
